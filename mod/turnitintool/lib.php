be5ba8ef55e6bb5275b79b1b0c91d3fb809fd0ed
Cathal O'Riordan <cathal@madein75.com>
2016-01-19 16:11:09 +0000

    dont backup turinitin tool ver 1 instances
    this module should be removed in future version

--- mod/turnitintool/lib.php
+++ mod/turnitintool/lib.php
@@ -62,7 +62,7 @@ function turnitintool_supports($feature) {
         case FEATURE_COMPLETION_TRACKS_VIEWS: return true;
         case FEATURE_GRADE_HAS_GRADE:         return true;
         case FEATURE_GRADE_OUTCOMES:          return true;
-        case FEATURE_BACKUP_MOODLE2:          return true;
+        case FEATURE_BACKUP_MOODLE2:          return false;
         case FEATURE_SHOW_DESCRIPTION:        return true;
 
         default: return null;
